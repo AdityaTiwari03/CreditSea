0fc776e1b84c728813e317d3aa71c2045c4c078d
AdityaTiwari03 <aadi.tiwari0208@gmail.com>
2024-10-10 12:53:31 +0530

    prototype

---
+++ backend/dist/models/Loan.js
@@ -0,0 +1,68 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Loan = void 0;
+const mongoose_1 = __importStar(require("mongoose"));
+// Create the Loan schema
+const LoanSchema = new mongoose_1.Schema({
+    idNumber: {
+        type: Number,
+        required: true,
+    },
+    fullName: {
+        type: String,
+        required: true,
+    },
+    loanAmount: {
+        type: Number,
+        required: true,
+    },
+    loanTenure: {
+        type: Number,
+        required: true,
+    },
+    employmentStatus: {
+        type: String,
+        required: true,
+    },
+    reasonForLoan: {
+        type: String,
+        required: true,
+    },
+    employmentAddress: {
+        type: String,
+        required: true,
+    },
+    status: {
+        type: String,
+        required: true,
+    },
+    loanOfficer: {
+        type: String,
+        required: true,
+    }
+}, { timestamps: true }); // Enable timestamps (createdAt, updatedAt)
+// Create and export the Loan model
+exports.Loan = mongoose_1.default.model('Loan', LoanSchema);
